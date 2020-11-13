63279a6d9b18e3d1c97776bf4468bfbe963f1b77
Benedikt Terhechte <benedikt.terhechte@xing.com>
2020-11-13 16:40:32 +0100

    Initial Commit

---
+++ NSSpain/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NSSpain
+//
+//  Created by Benedikt Terhechte on 13.11.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NSSpain/NSSpainApp.swift
@@ -0,0 +1,17 @@
+//
+//  NSSpainApp.swift
+//  NSSpain
+//
+//  Created by Benedikt Terhechte on 13.11.20.
+//
+
+import SwiftUI
+
+@main
+struct NSSpainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
